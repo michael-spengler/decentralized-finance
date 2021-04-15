1e08b5b0e650c970430fccd17623f223335e9eeb
Michael <michael.spengler@sap.com>
2021-04-15 02:45:33 +0200

    ex

--- src/gambling-strategies/low-brainer-based-using-binance/universal.ts
+++ src/gambling-strategies/low-brainer-based-using-binance/universal.ts
@@ -1,26 +1,22 @@
+
 import { BinanceConnector } from "../../binance/binance-connector"
 
-const intervalLengthInSeconds = Number(process.argv[2])
-const size = Number(process.argv[3])
-const threshold = Number(process.argv[4])
+const intervalLengthInSeconds = Number(process.argv[2]) // e.g. 5
+const size = Number(process.argv[3]) // e.g. 0.02
+const threshold = Number(process.argv[4]) // e.g. 2
 const binanceApiKey = process.argv[5]
 const binanceApiSecret = process.argv[6]
-const pair = process.argv[7]
-const limitRegardingBuyingTheDip = Number(process.argv[8])
-const dipIndicator = Number(process.argv[9])
-const buyTheDipFactor = Number(process.argv[10])
+const pair = process.argv[7] // e.g. BTCUSDT 
+const limitRegardingBuyingTheDip = Number(process.argv[8]) // e.g. 0.85
+const dipIndicator = Number(process.argv[9]) // e.g. 1
+const buyTheDipFactor = Number(process.argv[10]) // e.g. 1
 
-let dipCounter = 0
-
-// best parameterization so far:
-// ts-node src/gambling-strategies/low-brainer-based-using-binance/universal.ts 5 1 2 <apiKey> <apiSecret> BNBUSDT 0.9 0.5 2
-// ts-node src/gambling-strategies/low-brainer-based-using-binance/universal.ts 5 0.01 2 <apiKey> <apiSecret> TCUSDT 0.9 0.3 2
-console.log(`intervalLengthInSeconds: ${intervalLengthInSeconds}`)
-console.log(`size: ${size}`)
-console.log(`threshold: ${threshold}`)
+console.log(`intervalLengthInSeconds: ${intervalLengthInSeconds}`) 
+console.log(`size: ${size}`) 
+console.log(`threshold: ${threshold}`) 
 console.log(`binanceApiKey: ${binanceApiKey}`)
 console.log(`binanceApiSecret: ${binanceApiSecret}`)
-console.log(`pair: ${pair}`)
+console.log(`pair: ${pair}`) 
 
 const binanceConnector = new BinanceConnector(binanceApiKey, binanceApiSecret)
 
@@ -29,9 +25,6 @@ setInterval(async () => {
     const accountData = await binanceConnector.getFuturesAccountData()
     const xPosition = accountData.positions.filter((entry: any) => entry.symbol === pair)[0]
     const liquidityRatio = Number(accountData.maxWithdrawAmount) / Number(accountData.totalWalletBalance)
-
-    // console.log(liquidityRatio)
-    // console.log(xPosition)
 
     const positionAmountAsNumber = Number(xPosition.positionAmt)
     const unrealizedProfitAsNumber = Number(xPosition.unrealizedProfit)
@@ -42,14 +35,12 @@ setInterval(async () => {
     } else {
         console.log(unrealizedProfitAsNumber)
         if (unrealizedProfitAsNumber >= threshold) {
-            console.log(`selling ${size} ${pair}`)
+            console.log(`selling ${xPosition.positionAmt} ${pair}`)
             await binanceConnector.sellFuture(pair, Number(xPosition.positionAmt))
-            // play sound
-            dipCounter = 0
+            // play sound - https://www.freesoundslibrary.com/cow-moo-sounds/ & https://www.npmjs.com/package/play-sound
         } else if ((unrealizedProfitAsNumber < (threshold * dipIndicator) * -1) && liquidityRatio > limitRegardingBuyingTheDip) {
-            console.log(`buying the dip with dipCounter: ${dipCounter}`)
-            dipCounter++
-            await binanceConnector.buyFuture(pair, ((size * dipCounter * buyTheDipFactor)))
+            console.log(`buying the dip`)
+            await binanceConnector.buyFuture(pair, ((size * buyTheDipFactor)))
         } else {
             console.log(`waiting until I made ${threshold} USD in profit`)
         }
