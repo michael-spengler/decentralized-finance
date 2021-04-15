2564797f4dd60c39804e3387360f38ff87d556c2
Michael <michael.spengler@sap.com>
2021-04-15 23:45:02 +0200

    just for fun

--- src/gambling-strategies/audio-success-indicators/player.ts
+++ src/gambling-strategies/audio-success-indicators/player.ts
@@ -1,18 +1,24 @@
-var player = require('play-sound')((opts: any) => { })
 
+var os = require('os')
+var player = require('play-sound')((opts: any) => { })
 
 export class Player {
 
-
     public static async playMP3(path: string) {
+        if (Player.shallIPlaySounds()) {
+            console.log(path)
 
-        console.log(path)
+            player.play(path, function (err: any) {
+                if (err) {
+                    console.log(err)
+                }
+            })
+        } else {
+            console.log(`I won't play sounds as I'm running on ${os.platform()} on a ${os.arch()} architecture.`)
+        }
+    }
 
-        player.play(path, function (err: any) {
-            if (err) {
-                console.log(err)
-            }
-        })
+    private static shallIPlaySounds(): boolean {
+        return (os.platform() === 'darwin' && os.arch() === 'x64') ? true : false
     }
 }
-
