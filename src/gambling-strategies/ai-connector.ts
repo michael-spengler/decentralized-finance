d11a63b490dd9ee0ff0ceda37c2266fee40c78a0
Michael <michael.spengler@sap.com>
2021-04-19 23:21:14 +0200

    ramble the gamble

---
+++ src/gambling-strategies/ai-connector.ts
@@ -0,0 +1,73 @@
+
+
+// Check https://github.com/DHBWMannheim/MachineLearning/blob/master/machineLearning.ipynb 
+
+import axios from "axios"
+
+export class AIConnector {
+
+    public static async isThereASeriousVetoFromATAPerspective(currentEtherPrice: number, pair: string): Promise<boolean> {
+
+        try {
+            const taForecastEntry = (await axios.get(`https://ml.aaronschweig.dev/technical/${pair.substr(0, 3)}-USD?days=1`)).data[1][0]
+            if (new Date() < new Date(taForecastEntry.date)) { // there was an error in the dependency - that's why I introduced this
+                const predictedETHPriceBasedOnTA = taForecastEntry.value
+                const percentage = ((predictedETHPriceBasedOnTA - currentEtherPrice) / currentEtherPrice) * 100
+                if (percentage > 0) {
+                    console.log(`The technical analysis indicates that the ${pair} price will rise by about ${percentage} percent`)
+                } else if (percentage < -2) {
+                    console.log(`The technical analysis indicates that the ${pair} price will decline by about ${percentage} percent. Even considering a buffer I suggest to sell the long position.`)
+                    return true
+                }
+
+                return false
+            }
+
+        } catch (error) {
+
+            console.log(`shit happened: ${error.message}`)
+
+        }
+
+        return false
+    }
+
+    public static async isThereASeriousVetoFromASAPerspective(): Promise<boolean> {
+
+        const sentiment = (await axios.get('https://ml.aaronschweig.dev/sentiment/twitter')).data
+
+        const referenceDate24HoursAgo = new Date(new Date().getTime() - 24 * 3600 * 1000)
+
+        let counter = 0
+        let sumSMA = 0
+        let sumValue = 0
+
+        while (counter < sentiment.length) {
+            sumSMA = sumSMA + sentiment[counter].sma
+            sumValue = sumValue + sentiment[counter].value
+            counter++
+        }
+
+        const averageSMA = sumSMA / sentiment.length
+        const averageValue = sumValue / sentiment.length
+
+        console.log(`averageSMA: ${averageSMA} - averageValue: ${averageValue}`)
+
+        let magicFactorSMA
+        let magicFactorValue
+        if (sentiment[0].date < referenceDate24HoursAgo) {
+            console.log(`${sentiment[0].date} is before ${referenceDate24HoursAgo} - which means not so many quality accounts post about it`)
+            magicFactorSMA = averageSMA - 0, 1
+            magicFactorValue = averageValue - 0, 1
+        } else {
+            magicFactorSMA = averageSMA + 0, 1
+            magicFactorValue = averageValue + 0, 1
+        }
+
+        if (magicFactorValue < 0.5 || magicFactorSMA < 0.5) {
+            console.log(`The sentiment analysis indicates that being long is a bad idea atm based on magicFactorSMA: ${magicFactorSMA} and magicFactorValue: ${magicFactorValue}`)
+        }
+
+        return false
+    }
+}
--- src/gambling-strategies/low-brainer-based-using-binance/keep-the-balance.ts
+++ src/gambling-strategies/low-brainer-based-using-binance/keep-the-balance.ts
@@ -1,52 +1,93 @@
 
 
 import { BinanceConnector } from "../../binance/binance-connector"
+import { AIConnector } from "../ai-connector"
 import { Player } from "../audio-success-indicators/player"
+
 
 const intervalLengthInSeconds = Number(process.argv[2]) // e.g. 11
 const pair = process.argv[3] // e.g. ETHUSDT 
-const ratioToBuy = Number(process.argv[4]) // e.g. 0.5
-const ratioToSell = Number(process.argv[5]) // e.g. 0.04
-const binanceApiKey = process.argv[6] // check your profile on binance.com --> API Management
-const binanceApiSecret = process.argv[7] // check your profile on binance.com --> API Management
+const leverageEffectWhichYouConfigured = Number(process.argv[4]) // e.g. 100 
+const couldBuyWouldBuyFactor = Number(process.argv[5]) // e.g. 0.1
+const minimumBuySize = Number(process.argv[6]) // e.g. 0.5 
+const sellPositionAmountFactor = Number(process.argv[7]) // e.g. 0.2 
+const ratioToBuy = Number(process.argv[8]) // e.g. 0.5
+const ratioToSell = Number(process.argv[9]) // e.g. 0.2
+const binanceApiKey = process.argv[10] // check your profile on binance.com --> API Management
+const binanceApiSecret = process.argv[11] // check your profile on binance.com --> API Management
 
 const binanceConnector = new BinanceConnector(binanceApiKey, binanceApiSecret)
 
+
 setInterval(async () => {
+
     const accountData = await binanceConnector.getFuturesAccountData()
     const xPosition = accountData.positions.filter((entry: any) => entry.symbol === pair)[0]
     const positionAmount = Number(xPosition.positionAmt)
     const totalWalletBalance = Number(accountData.totalWalletBalance)
-    const availableBalance = Number(accountData.availableBalance) 
-    const liquidityRatio = availableBalance / totalWalletBalance
+    const available = Number(accountData.availableBalance)
+    const liquidityRatio = available / totalWalletBalance
 
-    // console.log(`positionAmount: ${positionAmount} ${pair}`)
-    // console.log(`totalPositionInitialMargin: ${accountData.totalPositionInitialMargin}`)
-    console.log(`walletBalance: ${totalWalletBalance} - unrealizedPNL: ${accountData.totalUnrealizedProfit} - availableBalance: ${availableBalance}`)
-    
+    console.log(`balance: ${totalWalletBalance} - unrealizedPNL: ${accountData.totalUnrealizedProfit} - available: ${available} - positionAmount: ${positionAmount} ${pair} - lr: ${liquidityRatio}`)
+
     const currentPrices = await binanceConnector.getCurrentPrices()
     const currentPrice = currentPrices.filter((e: any) => e.coinSymbol === pair)[0].price
-
-    const howMuchCouldIBuy = currentPrice / (totalWalletBalance * 125)
-    const howMuchShouldIBuy = Number((howMuchCouldIBuy / 10).toFixed(3))
-
-    if (liquidityRatio >= ratioToBuy && howMuchShouldIBuy > 0.5) { 
-
-        console.log(`I could buy ${howMuchCouldIBuy} ${pair} - I buy ${howMuchShouldIBuy} ${pair}`)
     
-        await binanceConnector.buyFuture(pair, howMuchShouldIBuy)
-        Player.playMP3(`${__dirname}/../../../sounds/game-new-level.mp3`) // https://www.freesoundslibrary.com/cow-moo-sounds/ 
+    const howMuchCouldIBuy = currentPrice / (available * leverageEffectWhichYouConfigured)
+    
+    if ((await AIConnector.isThereASeriousVetoFromATAPerspective(currentPrice, pair)))  {
+
+        if (positionAmount > 0) {
+            console.log(`I'm selling the long position as our the automated technical analysis raised some concerns.`)
+            await binanceConnector.sellFuture(pair,(positionAmount))
+        } else {
+            console.log(`waiting until the technical analysis indicates going long again.`)
+        }
+
+    } else if (await AIConnector.isThereASeriousVetoFromASAPerspective()) {
+
+        if (positionAmount > 0) {
+            console.log(`I'm selling the long position as our the automated sentiment analysis raised some concerns.`)
+            await binanceConnector.sellFuture(pair, (positionAmount))
+        } else {
+            console.log(`waiting until the sentiment analysis indicates going long again.`)
+        }
+
+    } else {
+        
+        console.log(`technical analysis, sentiment analysis and personal fundamentals analysis combined indicate being long on ${pair}.`)
+
+        if (liquidityRatio >= ratioToBuy) {
+
+            console.log(`I could buy ${howMuchCouldIBuy} ${pair}`)
+
+            const howMuchShouldIBuy = Number((howMuchCouldIBuy * couldBuyWouldBuyFactor).toFixed(3))
+
+            if (howMuchShouldIBuy > minimumBuySize) {
+                console.log(`I buy ${howMuchShouldIBuy} ${pair}`)
+
+                await binanceConnector.buyFuture(pair, howMuchShouldIBuy)
+                Player.playMP3(`${__dirname}/../../../sounds/game-new-level.mp3`) // https://www.freesoundslibrary.com
+            } else {
+                console.log(`I wait until I reach the minimum buy size of ${minimumBuySize} ${pair}. Currently the howMuchShouldIBuy amount is: ${howMuchShouldIBuy}`)
+            }
+        }
+
+        if (liquidityRatio <= ratioToSell) {
+
+            const howMuchShouldISell = Number((positionAmount * sellPositionAmountFactor).toFixed(3))
+            await binanceConnector.sellFuture(pair, howMuchShouldISell)
+            console.log(`selling ${howMuchShouldISell} ${pair}.`)
+            Player.playMP3(`${__dirname}/../../../sounds/cow-moo-sound.mp3`) // https://www.freesoundslibrary.com/cow-moo-sounds/ 
+
+        }
+
+        if (liquidityRatio > ratioToSell && liquidityRatio < ratioToBuy) {
+
+            console.log(`it seems we are reasonably invested with a liquidity ratio of ${liquidityRatio}.`)
+
+        }
     }
 
-    if (liquidityRatio <= ratioToSell) {
-        const howMuchShouldISell = Number((positionAmount * 0.2).toFixed(3))
-        await binanceConnector.sellFuture(pair, howMuchShouldISell)
-        console.log(`selling ${howMuchShouldISell} ${pair}.`)
-        Player.playMP3(`${__dirname}/../../../sounds/cow-moo-sound.mp3`) // https://www.freesoundslibrary.com/cow-moo-sounds/ 
-    } 
-    
-    if (liquidityRatio > ratioToSell && liquidityRatio < ratioToBuy) {
-        console.log(`it seems we are reasonably invested with a liquidity ratio of ${liquidityRatio}.`)
-    }
-    
 }, intervalLengthInSeconds * 1000)
+
