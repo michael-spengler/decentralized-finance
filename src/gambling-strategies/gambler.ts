ad157bd414526e9afb7abd8e3288ce083ce139b4
Michael <michael.spengler@sap.com>
2021-04-25 01:35:48 +0200

    improve order of parameters

--- src/gambling-strategies/gambler.ts
+++ src/gambling-strategies/gambler.ts
@@ -42,12 +42,14 @@ export class Gambler {
         const liquidityRatio = accountData.availableBalance / accountData.totalWalletBalance
         const lowestPrice80_100 = this.portfolioProvider.getLowestPriceOfRecentXIntervals(80, 100)
         const highestPrice3_8 = this.portfolioProvider.getHighestPriceOfRecentXIntervals(3, 8)
-       
+
         console.log(`LR: ${liquidityRatio}; CPP: ${cPP}; lP80_100: ${lowestPrice80_100}; hP3_8: ${highestPrice3_8} nyrPNL: ${accountData.totalUnrealizedProfit}; `)
 
-        
-        if (liquidityRatio <= this.liquidityRatioToSell) {
-            if (liquidityRatio <= (this.liquidityRatioToSell * 0.9)){
+        if (Number(accountData.totalWalletBalance) <= this.reinvestAt) {
+            console.log(`I reinvest e.g. after a serious drop`)
+            await this.reinvestAfterASeriousDrop()
+        } else if (liquidityRatio <= this.liquidityRatioToSell) {
+            if (liquidityRatio <= (this.liquidityRatioToSell * 0.9)) {
                 await this.sell(0.8)
                 console.log(`selling 95% of assets as it looks like a very strong dip`)
             } else {
@@ -55,7 +57,7 @@ export class Gambler {
                 await this.sell(0.5)
             }
         } else if (liquidityRatio >= this.liquidityRatioToBuy) {
-            if (cPP === lowestPrice80_100){
+            if (cPP === lowestPrice80_100) {
                 await this.buy(currentPrices, accountData)
                 await this.saveSomething(accountData)
             } else {
@@ -65,45 +67,42 @@ export class Gambler {
             await this.buy(currentPrices, accountData)
             console.log(`A surprisingly consistent uprise seems to take place - not waiting for the relative dip any longer - buying right now :)`)
             await this.saveSomething(accountData)
-        } else if ((((this.liquidityRatioToBuy +  this.liquidityRatioToSell) / 2) > liquidityRatio)){
+        } else if ((((this.liquidityRatioToBuy + this.liquidityRatioToSell) / 2) > liquidityRatio)) {
             if (cPP < highestPrice3_8) {
                 console.log(`I'm seeking to reduce my risk as soon as soon as I hit the highest relative price.`)
             } else {
                 console.log(`gently reducing the risk by selling 10%`)
                 await this.sell(0.1)
             }
-        } else if(Number(accountData.totalUnrealizedProfit) > Number(accountData.totalWalletBalance)) {
+        } else if (Number(accountData.totalUnrealizedProfit) > Number(accountData.totalWalletBalance)) {
             console.log(`Selling and saving something as I made some significant gains.`)
             console.log(`${accountData.totalUnrealizedProfit} vs. ${accountData.totalWalletBalance}`)
             await this.sell(0.1)
             await this.saveSomething(accountData)
-        } else if (Number(accountData.totalWalletBalance) <= this.reinvestAt) {
-            console.log(`I reinvest after a serious drop`)
-            await this.reinvestAfterASeriousDrop()
         } else {
             console.log(`I'm reasonably invested. LR: ${liquidityRatio}; TWB: ${accountData.totalWalletBalance}`)
         }
-         
+
     }
 
-    private async saveSomething(accountData: any){
+    private async saveSomething(accountData: any) {
         const savingsAmount = Number((accountData.availableBalance * 0.05).toFixed(0))
-        if (savingsAmount >= 1)  {
+        if (savingsAmount >= 1) {
             console.log(`I'll transfer ${savingsAmount} USDT to my fiat and spot account to prepare for a reinvestment after a serious drop.`)
             try {
                 await this.binanceConnector.transferFromUSDTFuturesToSpotAccount(savingsAmount)
-            } catch(error){
+            } catch (error) {
                 console.log(`I could not save something as I got the error: ${error.message}`)
             }
         }
     }
-    
+
     private async reinvestAfterASeriousDrop() {
         try {
             await this.binanceConnector.transferFromSpotAccountToUSDTFutures(this.investmentAmount)
-        } catch(error){
+        } catch (error) {
             console.log(`I could not reinvest as I got the error: ${error.message}`)
-        }        
+        }
     }
 
     private async buy(currentPrices: any[], accountData: any) {
--- src/gambling-strategies/test-gambler.ts
+++ src/gambling-strategies/test-gambler.ts
@@ -2,8 +2,8 @@ import { Gambler } from "./gambler"
 
 const lrToBuy = Number(process.argv[2]) // e.g. 0.9
 const lrToSell = Number(process.argv[3]) // e.g. 0.4
-const investmentAmount = Number(process.argv[4]) // e.g. 20
-const reinvestAt = Number(process.argv[5]) // 10
+const reinvestAt = Number(process.argv[4]) // 10
+const investmentAmount = Number(process.argv[5]) // e.g. 20
 const binanceApiKey = process.argv[6] // check your profile on binance.com --> API Management
 const binanceApiSecret = process.argv[7] // check your profile on binance.com --> API Management
 
