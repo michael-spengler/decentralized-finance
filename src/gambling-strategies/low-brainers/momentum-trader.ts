8a77fb622704f4c39e7ba5cd85ea954a6e9d4528
Michael <michael.spengler@sap.com>
2021-05-23 00:42:11 +0200

    simplify things

--- src/gambling-strategies/low-brainers/momentum-trader.ts
+++ src/gambling-strategies/low-brainers/momentum-trader.ts
@@ -22,9 +22,7 @@ export class Sprinter2 {
         const currentPrices = await this.binanceConnector.getCurrentPrices()
         const currentPrice = this.portfolioProvider.getCurrentXPrice(currentPrices, this.pair)
         const accountData = await this.binanceConnector.getFuturesAccountData()
-        const xPosition = accountData.positions.filter((entry: any) => entry.symbol === this.pair)[0]
-        // const cPP = this.portfolioProvider.getCurrentPortfolioAveragePrice(currentPrices)
-        
+       
         // console.log(`currentPrice: ${currentPrice}`)
 
         if (this.historicData.length === 500000) {
@@ -32,9 +30,6 @@ export class Sprinter2 {
         }
 
         this.historicData.unshift(currentPrice)
-
-        const lowestPriceSince = this.getIsLowestSinceX(currentPrice)
-        const highestPriceSince = this.getIsHighestSinceX(currentPrice)
 
         const delta = this.historicData[1] - currentPrice
         
@@ -46,7 +41,7 @@ export class Sprinter2 {
             await this.binanceConnector.transferFromUSDTFuturesToSpotAccount(5)
         }
         
-        console.log(x)
+        // console.log(x)
         if (x > 0.1) {
             const theFactor = Number((x * 30).toFixed(0))
             console.log(theFactor)
@@ -75,69 +70,21 @@ export class Sprinter2 {
             if (position.positionAmt > 0) {
                 await this.binanceConnector.sellFuture(this.pair, position.positionAmt)
             } else if (position.positionAmt < 0) {
-                await this.binanceConnector.buyFuture(this.pair, position.positionAmt)
+                await this.binanceConnector.buyFuture(this.pair, Number(position.positionAmt) * -1)
             }
         } else {
-            // console.log(`pricePerTradingUnit: ${pricePerTradingUnit} - previousPrice: ${this.historicData[1]} - currentPrice: ${currentPrice}`)
-            // console.log('boring times')
-            // console.log(`lS: ${lowestPriceSince} - hS: ${highestPriceSince} - unrP: ${xPosition.unrealizedProfit} - avB: ${accountData.availableBalance} - posAmt: ${xPosition.positionAmt}`)
-            // const pricePerTradingUnit = currentPrice * this.tradingUnit
-            // console.log(pricePerTradingUnit)
+            console.log(`boring times - avB: ${accountData.availableBalance}`)
         }
-
-        // console.log(x)
-        // if (Number(accountData.availableBalance) < 2) {
-        //     console.log(`time to sell some of the assets`)
-        //     this.binanceConnector.sellFuture(pair, Number((Number(xPosition.positionAmt) * 0.8).toFixed(this.decimalPlacesOfPair)))
-        // } else {
-        //     if ((lowestPriceSince >= 300 || this.historicData.length === 1) && Number(xPosition.positionAmt) > this.tradingUnit * 8 * -1 && Number(accountData.availableBalance) > pricePerTradingUnit / 5) {
-        //         console.log(`time to go long`)
-        //         this.binanceConnector.buyFuture(pair, this.tradingUnit)
-        //     } else if (Number(accountData.availableBalance) > pricePerTradingUnit / 2) {
-        //         console.log(`time to save something`)
-        //         await this.binanceConnector.transferFromUSDTFuturesToSpotAccount(5)
-        //     } else {
-        //         console.log(`boring times`)
-        //     }
-        // }
 
         this.previousPNL = Number(accountData.totalUnrealizedProfit)
 
     }
-
-    private getIsLowestSinceX(price: number) {
-        let counter = 0
-
-        for (const e of this.historicData) {
-            if (price > e) {
-                return counter
-            }
-            counter++
-        }
-        return counter
-    }
-
-    private getIsHighestSinceX(price: number) {
-        let counter = 0
-
-        for (const e of this.historicData) {
-            if (price < e) {
-                return counter
-            }
-            counter++
-        }
-        return counter
-    }
-
 
 }
 
 
 const binanceApiKey = process.argv[2] // check your profile on binance.com --> API Management
 const binanceApiSecret = process.argv[3] // check your profile on binance.com --> API Management
-
-// const pair = 'ETHUSDT'
-// const tradingUnit = 0.007
 
 const pair = 'BTCUSDT'
 const tradingUnit = 0.001
@@ -149,6 +96,5 @@ const jumpStarter = new Sprinter2(binanceApiKey, binanceApiSecret, tradingUnit, 
 setInterval(async () => {
 
     jumpStarter.investWisely()
-
 
 }, 11 * 1000)
