cf824659bb610533129ed58e1fd24bdc7552535e
Michael <michael.spengler@sap.com>
2021-07-29 03:21:02 +0200

    fun

--- src/gambling-strategies/low-brainers/harmony.ts
+++ src/gambling-strategies/low-brainers/harmony.ts
@@ -94,7 +94,7 @@ export class Harmony {
                 // check if I should close the deal or potentially act in beast mode
                 if ((unrealizedProfitInPercent > sellingAt ||
                     unrealizedProfitInPercent < - 54 ||
-                    unrealizedProfitInPercent >= 9 && this.investmentSpecificCounter % 9 === 0) &&
+                    unrealizedProfitInPercent >= 18 && this.investmentSpecificCounter % 9 === 0) &&
                     Number(bitcoinPosition.positionAmt) > 0) {
 
                     console.log(`closing the deal with an unrealizedProfitInPercent of ${unrealizedProfitInPercent}% - eskalationsStufe: ${this.eskalationsStufe} - bitCoinAverageDeltaInPercent ${bitCoinAverageDeltaInPercent}`)
