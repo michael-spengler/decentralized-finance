0b228885b4e6b6e4053eaea395ea5dfc71333199
Michael <michael.spengler@sap.com>
2021-08-17 02:08:09 +0200

    automated commit

--- src/gambling-strategies/low-brainers/two-accounts.ts
+++ src/gambling-strategies/low-brainers/two-accounts.ts
@@ -5,42 +5,31 @@ export class TwoAccountsExploit {
 
     private accountData1: any
     private accountData2: any
-
     private marginRatio1 = 0
     private marginRatio2 = 0
-
     private bitcoinPosition1: any
     private bitcoinPosition2: any
-
     private dogePosition1: any
     private dogePosition2: any
-
     private pnlBTC1 = 0
     private pnlBTC2 = 0
-
     private pnlDOGE1 = 0
     private pnlDOGE2 = 0
-
-    private bitcoinInvestmentAmount = 0.01
-    private dogeInvestmentAmount = 100
-
+    private bitcoinInvestmentAmount = 0.002
+    private dogeInvestmentAmount = 20
     private closingAt = 0
-
     private addingAt = 0
-
     private iterationCounter = 0
-
     private startBalanceUnderRisk = 0
-
     private pauseCounter = 0
-
     private balanceUnderRisk = 0
-
     private totalUnrealizedProfitInPercent1 = 0
     private totalUnrealizedProfitInPercent2 = 0
     private totalUnrealizedProfitInPercent = 0
-
     private failureCount = 0
+    private level = 0
+    private profitInCurrent4To0Session = 0
+
 
     public constructor(private readonly binanceConnector1: BinanceConnector, private readonly binanceConnector2: BinanceConnector) {
         this.binanceConnector1 = binanceConnector1
@@ -76,65 +65,189 @@ export class TwoAccountsExploit {
         } else {
 
             if (this.pauseCounter > 0) {
+
                 console.log(`\nI pause as there has been an extreme move by the centralized forces recently - pauseCounter: ${this.pauseCounter}`)
+
+            } else if (this.getNumberOfOpenPositions() === 4 || this.getNumberOfOpenPositions() === 3) {
+
+                this.level = 1
+
+                await this.optimizePositionsLevel1()
+
+
+            } else if (this.getNumberOfOpenPositions() === 2 && this.level === 1) {
+
+                await this.getRidOfTheSecondWorst()
+
+            } else if (this.getNumberOfOpenPositions() === 1 && this.level === 1) {
+
+                await this.establishAMeanHedge()
+
+                this.level = 2
+
+            } else if (this.getNumberOfOpenPositions() === 2 && this.level === 2) {
+
+                if (await this.couldWeCloseAPosition()) {
+
+                    await this.closeAllOpenPositions()
+
+                }
+
+            } else if (this.getNumberOfOpenPositions() === 1 && this.level === 2 || this.level === 0) {
+                
+                console.log(`Hmm - probably even the badest position has a pnl > 0 --> closing`)
+                await this.closeAllOpenPositions()
+
             } else {
-                console.log(`\nI optimize ${this.getNumberOfOpenPositions()} open positions`)
-                await this.optimizePositions()
+
+                console.log(`Hmm - how can I have ${this.getNumberOfOpenPositions()} and play in level ${this.level}?`)
+
             }
 
         }
     }
 
+    private async establishAMeanHedge(): Promise<void> {
 
-    private async optimizePositions(): Promise<void> {
+        console.log(`I establish a mean hedge if the position which is left has a pnl of < 0`)
 
+        if (this.isLowestPNL(this.pnlBTC1) && this.pnlBTC1 < 0) {
+            console.log(`I'll hedge the least successful position --> adding to BTC2.`)
+            await this.addToBTC2(Number((this.bitcoinPosition1.positionAmt * 1.1).toFixed(3)))
+        }
+        if (this.isLowestPNL(this.pnlBTC2) && this.pnlBTC2 < 0) {
+            console.log(`I'll hedge the least successful position --> adding to BTC1.`)
+            await this.addToBTC1(Number((this.bitcoinPosition2.positionAmt * 1.1).toFixed(3)) * -1)
+        }
+        if (this.isLowestPNL(this.pnlDOGE1) && this.pnlDOGE1 < 0) {
+            console.log(`I'll hedge the least successful position --> adding to DOGE2.`)
+            await this.addToDOGE2(Number((this.dogePosition1.positionAmt * 1.1).toFixed(0)) * -1)
+        }
+        if (this.isLowestPNL(this.pnlDOGE2) && this.pnlDOGE2 < 0) {
+            console.log(`I'll hedge the least successful position --> adding to DOGE1.`)
+            await this.addToDOGE1(Number((this.dogePosition2.positionAmt * 1.1).toFixed(0)))
+        }
+    }
+
+    private async getRidOfTheSecondWorst(): Promise<void> {
+        if (this.getNumberOfOpenPositions() !== 2) {
+            throw new Error("this only works if the number of open positions is 2")
+        }
+
+        console.log(`I get rid of the second worst position ${Number(this.bitcoinPosition1.unrealizedProfit)}`)
+
+        if (this.isHighestAbsolute(this.bitcoinPosition1.unrealizedProfit)) {
+            console.log(`getting rid of BTC1 as it is the second worst`)
+            await this.closeBTC1()
+        } else if (this.isHighestAbsolute(this.bitcoinPosition2.unrealizedProfit)) {
+            console.log(`getting rid of BTC2 as it is the second worst`)
+            await this.closeBTC2()
+        } else if (this.isHighestAbsolute(this.dogePosition1.unrealizedProfit)) {
+            console.log(`getting rid of DOGE1 as it is the second worst`)
+            await this.closeDOGE1()
+        } else if (this.isHighestAbsolute(this.dogePosition2.unrealizedProfit)) {
+            console.log(`getting rid of DOGE2 as it is the second worst`)
+            await this.closeDOGE2()
+        }
+    }
+
+    private async couldWeCloseAPosition(): Promise<boolean> {
         if (this.totalUnrealizedProfitInPercent > this.closingAt) {
 
             await this.closeAllOpenPositions()
 
-        } else if (this.pnlBTC1 > this.closingAt) {
+            return true
+
+        } else if (this.pnlBTC1 > this.closingAt && Number(this.bitcoinPosition1.unrealizedProfit) > Number(this.accountData1.totalWalletBalance) / 20) {
 
             await this.closeBTC1()
+            return true
 
-        } else if (this.pnlBTC2 > this.closingAt) {
+        } else if (this.pnlBTC2 > this.closingAt && Number(this.bitcoinPosition2.unrealizedProfit) > Number(this.accountData2.totalWalletBalance) / 20) {
 
             await this.closeBTC2()
+            return true
 
-        } else if (this.pnlDOGE1 > this.closingAt) {
+        } else if (this.pnlDOGE1 > this.closingAt && Number(this.dogePosition1.unrealizedProfit) > Number(this.accountData1.totalWalletBalance) / 20) {
 
             await this.closeDOGE1()
+            return true
 
-        } else if (this.pnlDOGE2 > this.closingAt) {
+        } else if (this.pnlDOGE2 > this.closingAt && Number(this.dogePosition2.unrealizedProfit) > Number(this.accountData2.totalWalletBalance) / 20) {
 
             await this.closeDOGE2()
+            return true
 
         } else {
+            return false
+        }
+
+    }
+
+    private async optimizePositionsLevel1(): Promise<void> {
+
+        console.log(`\nI optimize ${this.getNumberOfOpenPositions()} open positions on level ${this.level}`)
+
+        if ((await this.couldWeCloseAPosition()) === false) {
 
             await this.accelerate()
 
         }
 
+
     }
+
+
 
     private async accelerate() {
 
-        if (this.isLowestPNL(this.pnlBTC1) && this.pnlBTC1 < this.addingAt && this.marginRatio1 < 45) {
+        if (this.pnlBTC1 < this.addingAt && this.marginRatio1 < 18) {
 
-            await this.addToBTC1()
-
-        } else if (this.isLowestPNL(this.pnlDOGE1) && this.pnlDOGE1 < this.addingAt && this.marginRatio1 < 45) {
-
-            await this.addToDOGE1()
-
-        } else if (this.isLowestPNL(this.pnlBTC2) && this.pnlBTC2 < this.addingAt && this.marginRatio2 < 45) {
-
-            await this.addToBTC2()
-
-        } else if (this.isLowestPNL(this.pnlDOGE2) && this.pnlDOGE2 < this.addingAt && this.marginRatio2 < 45) {
-
-            await this.addToDOGE2()
+            await this.addToBTC1(this.bitcoinInvestmentAmount)
 
         }
+
+        if (this.pnlDOGE1 < this.addingAt && this.marginRatio1 < 18) {
+
+            await this.addToDOGE1(this.dogeInvestmentAmount)
+
+        } 
+        
+        if (this.pnlBTC2 < this.addingAt && this.marginRatio2 < 18) {
+
+            await this.addToBTC2(this.bitcoinInvestmentAmount)
+
+        }
+
+        if (this.pnlDOGE2 < this.addingAt && this.marginRatio2 < 18) {
+
+            await this.addToDOGE2(this.dogeInvestmentAmount)
+
+        }
+
+    }
+
+    private isLowestAbsolute(absolutPNLUnderObservation: number): boolean {
+        if (absolutPNLUnderObservation <= Number(this.bitcoinPosition1.unrealizedProfit) &&
+            absolutPNLUnderObservation <= Number(this.bitcoinPosition2.unrealizedProfit) &&
+            absolutPNLUnderObservation <= Number(this.dogePosition1.unrealizedProfit) &&
+            absolutPNLUnderObservation <= Number(this.dogePosition2.unrealizedProfit)) {
+            return true
+        }
+
+        return false
+
+    }
+
+    private isHighestAbsolute(absolutPNLUnderObservation: number): boolean {
+        if (absolutPNLUnderObservation >= Number(this.bitcoinPosition1.unrealizedProfit) &&
+            absolutPNLUnderObservation >= Number(this.bitcoinPosition2.unrealizedProfit) &&
+            absolutPNLUnderObservation >= Number(this.dogePosition1.unrealizedProfit) &&
+            absolutPNLUnderObservation >= Number(this.dogePosition2.unrealizedProfit)) {
+            return true
+        }
+
+        return false
 
     }
 
@@ -154,72 +267,77 @@ export class TwoAccountsExploit {
         await this.binanceConnector1.buyFuture('BTCUSDT', this.bitcoinInvestmentAmount)
     }
 
-    private async addToBTC1(): Promise<void> {
-        console.log(`Adding ${this.bitcoinPosition1.positionAmt} BTC to BTC1.`)
-        await this.binanceConnector1.buyFuture('BTCUSDT', this.bitcoinPosition1.positionAmt)
+    private async addToBTC1(amount: number = this.bitcoinPosition1.positionAmt): Promise<void> {
+        console.log(`Adding ${amount} BTC to BTC1.`)
+        await this.binanceConnector1.buyFuture('BTCUSDT', amount)
     }
 
     private async closeBTC1(): Promise<void> {
 
         if (this.pnlBTC1 > 0) {
-            console.log(`Yay you just made ${this.pnlBTC1}% profit from BTC1.`)
+            console.log(`Yay you just made ${Number(this.bitcoinPosition1.unrealizedProfit)} (${this.pnlBTC1}%) profit from BTC1.`)
         }
 
         await this.binanceConnector1.sellFuture('BTCUSDT', this.bitcoinPosition1.positionAmt)
+        this.profitInCurrent4To0Session = this.profitInCurrent4To0Session + Number(this.bitcoinPosition1.unrealizedProfit)
     }
 
     private async openBTC2(): Promise<void> {
         await this.binanceConnector2.sellFuture('BTCUSDT', this.bitcoinInvestmentAmount)
     }
 
-    private async addToBTC2(): Promise<void> {
-        console.log(`Adding ${this.bitcoinPosition2.positionAmt} BTC to BTC2.`)
-        await this.binanceConnector2.sellFuture('BTCUSDT', this.bitcoinPosition2.positionAmt * -1)
+    private async addToBTC2(amount: number = this.bitcoinPosition2.positionAmt * -1): Promise<void> {
+        console.log(`Adding ${amount} BTC to BTC2.`)
+        await this.binanceConnector2.sellFuture('BTCUSDT', amount)
     }
 
     private async closeBTC2(): Promise<void> {
 
         if (this.pnlBTC2 > 0) {
-            console.log(`Yay you just made ${this.pnlBTC2}% profit from BTC2.`)
+            console.log(`Yay you just made ${Number(this.bitcoinPosition2.unrealizedProfit)} (${this.pnlBTC2}%) profit from BTC2.`)
         }
 
         await this.binanceConnector2.buyFuture('BTCUSDT', this.bitcoinPosition2.positionAmt * -1)
+        this.profitInCurrent4To0Session = this.profitInCurrent4To0Session + Number(this.bitcoinPosition2.unrealizedProfit)
     }
 
     private async openDOGE1(): Promise<void> {
         await this.binanceConnector1.sellFuture('DOGEUSDT', this.dogeInvestmentAmount)
     }
 
-    private async addToDOGE1(): Promise<void> {
-        console.log(`Adding ${this.dogePosition1.positionAmt} DOGE to DOGE1.`)
-        await this.binanceConnector1.sellFuture('DOGEUSDT', this.dogePosition1.positionAmt * -1)
+    private async addToDOGE1(amount: number = this.dogePosition1.positionAmt * -1): Promise<void> {
+        console.log(`Adding ${amount} DOGE to DOGE1.`)
+        await this.binanceConnector1.sellFuture('DOGEUSDT', amount)
     }
 
     private async closeDOGE1(): Promise<void> {
 
         if (this.pnlDOGE1 > 0) {
-            console.log(`Yay you just made ${this.pnlDOGE1}% profit from DOGE1.`)
+            console.log(`Yay you just made ${Number(this.dogePosition1.unrealizedProfit)} (${this.pnlDOGE1}%) profit from DOGE1.`)
         }
 
         await this.binanceConnector1.buyFuture('DOGEUSDT', this.dogePosition1.positionAmt * -1)
+        this.profitInCurrent4To0Session = this.profitInCurrent4To0Session + Number(this.dogePosition1.unrealizedProfit)
+
     }
 
     private async openDOGE2(): Promise<void> {
         await this.binanceConnector2.buyFuture('DOGEUSDT', this.dogeInvestmentAmount)
     }
 
-    private async addToDOGE2(): Promise<void> {
-        console.log(`Adding ${this.dogePosition2.positionAmt} DOGE to DOGE2.`)
-        await this.binanceConnector2.buyFuture('DOGEUSDT', this.dogePosition2.positionAmt)
+    private async addToDOGE2(amount: number = this.dogePosition2.positionAmt): Promise<void> {
+        console.log(`Adding ${amount} DOGE to DOGE2.`)
+        await this.binanceConnector2.buyFuture('DOGEUSDT', amount)
     }
 
     private async closeDOGE2(): Promise<void> {
 
         if (this.pnlDOGE2 > 0) {
-            console.log(`Yay you just made ${this.pnlDOGE2}% profit from DOGE2.`)
+            console.log(`Yay you just made ${Number(this.dogePosition2.unrealizedProfit)} (${this.pnlDOGE2}%) profit from DOGE2.`)
         }
 
         await this.binanceConnector2.sellFuture('DOGEUSDT', this.dogePosition2.positionAmt)
+        this.profitInCurrent4To0Session = this.profitInCurrent4To0Session + Number(this.dogePosition2.unrealizedProfit)
     }
 
     private async startTheGame(): Promise<void> {
@@ -264,6 +382,7 @@ export class TwoAccountsExploit {
         if (this.iterationCounter === 1) {
             this.startBalanceUnderRisk = Number(this.accountData1.totalWalletBalance) + Number(this.accountData2.totalWalletBalance) + Number(this.accountData2.totalUnrealizedProfit)
         }
+
         this.balanceUnderRisk = Number(this.accountData1.totalWalletBalance) + Number(this.accountData2.totalWalletBalance) + Number(this.accountData1.totalUnrealizedProfit) + Number(this.accountData2.totalUnrealizedProfit)
 
         this.marginRatio1 = Number(this.accountData1.totalMaintMargin) * 100 / Number(this.accountData1.totalMarginBalance)
@@ -302,9 +421,9 @@ export class TwoAccountsExploit {
         this.totalUnrealizedProfitInPercent2 = (Number(this.accountData2.totalUnrealizedProfit) * 100 / Number(this.accountData2.totalWalletBalance))
         this.totalUnrealizedProfitInPercent = this.totalUnrealizedProfitInPercent1 + this.totalUnrealizedProfitInPercent1
 
-        console.log(`startBalUnderRisk: ${this.startBalanceUnderRisk} - balUnderRisk: ${this.balanceUnderRisk} - addingAt: ${this.addingAt} - closingAt: ${this.closingAt} - failureCount: ${this.failureCount}`)
+        console.log(`startBalUnderRisk: ${this.startBalanceUnderRisk} - balUnderRisk: ${this.balanceUnderRisk} - failureCount: ${this.failureCount} - addingAt: ${this.addingAt} - closingAt: ${this.closingAt} - profitInCurrent4To0Session: ${this.profitInCurrent4To0Session}`)
 
-        console.log(`\nPNL1: ${this.totalUnrealizedProfitInPercent1} - PNL2: ${this.totalUnrealizedProfitInPercent2} - PNL: ${this.totalUnrealizedProfitInPercent} - pnlBTC1: ${this.pnlBTC1} - pnlBTC2: ${this.pnlBTC2} - pnlDOGE1: ${this.pnlDOGE1} - pnlDOGE2: ${this.pnlDOGE2}`)
+        console.log(`\nPNL: ${this.totalUnrealizedProfitInPercent} - PNL1: ${this.totalUnrealizedProfitInPercent1} - PNL2: ${this.totalUnrealizedProfitInPercent2} - pnlBTC1: ${this.pnlBTC1} - pnlBTC2: ${this.pnlBTC2} - pnlDOGE1: ${this.pnlDOGE1} - pnlDOGE2: ${this.pnlDOGE2}`)
 
     }
 
@@ -321,23 +440,11 @@ export class TwoAccountsExploit {
 
     private async closeAllOpenPositions(): Promise<void> {
 
-        if (Number(this.bitcoinPosition1.positionAmt > 0)) {
-            await this.closeBTC1()
-        }
+        await this.sellAllLongPositions()
+        await this.sellAllShortPositions()
 
-        if (Number(this.dogePosition1.positionAmt < 0)) {
-            await this.closeDOGE1()
-        }
+        this.profitInCurrent4To0Session = 0
 
-        await this.sleep((Math.floor(Math.random() * (1000 - 100 + 1) + 100))) // staying undercover
-
-        if (Number(this.bitcoinPosition2.positionAmt < 0)) {
-            await this.closeBTC2()
-        }
-
-        if (Number(this.dogePosition2.positionAmt > 0)) {
-            await this.closeDOGE2()
-        }
     }
 
     private sleep(ms: number) {
@@ -345,6 +452,43 @@ export class TwoAccountsExploit {
             setTimeout(resolve, ms);
         });
     }
+
+    private async sellAllLongPositions() {
+        for (let position of this.accountData1.positions) {
+            if (position.positionAmt > 0) {
+                console.log(`selling ${position.symbol}`)
+                await this.binanceConnector1.sellFuture(position.symbol, Number(position.positionAmt))
+            }
+        }
+
+        await this.sleep((Math.floor(Math.random() * (100 - 10 + 1) + 10))) // staying undercover
+
+        for (let position of this.accountData2.positions) {
+            if (position.positionAmt > 0) {
+                console.log(`selling ${position.symbol}`)
+                await this.binanceConnector2.sellFuture(position.symbol, Number(position.positionAmt))
+            }
+        }
+    }
+
+    private async sellAllShortPositions() {
+        for (let position of this.accountData1.positions) {
+            if (position.positionAmt < 0) {
+                console.log(`buying ${position.symbol}`)
+                await this.binanceConnector1.buyFuture(position.symbol, Number(position.positionAmt))
+            }
+        }
+
+        await this.sleep((Math.floor(Math.random() * (100 - 10 + 1) + 10))) // staying undercover
+
+        for (let position of this.accountData2.positions) {
+            if (position.positionAmt < 0) {
+                console.log(`buying ${position.symbol}`)
+                await this.binanceConnector2.buyFuture(position.symbol, Number(position.positionAmt))
+            }
+        }
+    }
+
 }
 
 
